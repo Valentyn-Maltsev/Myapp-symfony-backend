63f63e99ff6adb23350e68e5cefc1401d0232ec5
Valentyn <valentynmaltsev@gmail.com>
2021-10-01 13:52:05 +0300

    Added images and fonts, common structure of templates

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -6,15 +6,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class DefaultController extends AbstractController
 {
     /**
      * @Route("/home", name="home")
      */
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+        return $this->render('main/default/index.html.twig', [
+            'controller_name' => 'DefaultController',
         ]);
     }
 }
