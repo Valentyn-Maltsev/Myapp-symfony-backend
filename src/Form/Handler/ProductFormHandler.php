756a9294a16b6a6c55a30910242e412ed2c15025
Valentyn <valentynmaltsev@gmail.com>
2021-11-12 18:07:16 +0200

    Resizing and loading images

--- src/Form/Handler/ProductFormHandler.php
+++ src/Form/Handler/ProductFormHandler.php
@@ -4,32 +4,33 @@ namespace App\Form\Handler;
 
 use App\Entity\Product;
 use App\Utils\File\FileSaver;
-use Doctrine\ORM\EntityManagerInterface;
+use App\Utils\Manager\ProductManager;
+use Doctrine\ORM\productManageInterface;
 use Symfony\Component\Form\Form;
 
 class ProductFormHandler
 {
     /**
-     * @var EntityManagerInterface
-     */
-    private $entityManager;
-
-    /**
      * @var FileSaver
      */
     private $fileSaver;
 
-    public function __construct(EntityManagerInterface $entityManager, FileSaver $fileSaver)
+    /**
+     * @var ProductManager
+     */
+    private $productManager;
+
+    public function __construct(ProductManager $productManager, FileSaver $fileSaver)
     {
-        $this->entityManager = $entityManager;
         $this->fileSaver = $fileSaver;
+        $this->productManager = $productManager;
     }
 
     public function processEditForm(Product $product, Form $form)
     {
         // ADD NEW IMAGE WITH DIFFERENT SIZES TO THE PRODUCT
         // 1. Save product's changes (+)
-        $this->entityManager->persist($product);
+        $this->productManager->save($product);
 
         $newImageFile = $form->get('newImage')->getData();
 
@@ -38,22 +39,15 @@ class ProductFormHandler
             ? $this->fileSaver->saveUploadedFileIntoTemp($newImageFile)
             : null;
 
-        dd($tempImageFilename);
-
-
         // 3. Work with product (addProductImage) and ProductImage
         // 3.1 Get path of folder with product images
-
         // 3.2 Work with ProductImage
         // 3.2.1 Resize and save image into folder (BIG, MIDDLE, SMALL)
         // 3.2.2 Create ProductImage and return it to Product
+        $this->productManager->updateProductImages($product, $tempImageFilename);
 
         // 3.3 Save Product with new ProductImage
-
-
-        dd($product, $form->get('newImage')->getData());
-
-        $this->entityManager->flush();
+        $this->productManager->save($product);
 
         return $product;
     }
