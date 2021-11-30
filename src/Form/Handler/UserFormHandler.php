004d2c69bbad6417d0c2c1ec5e58564cdcc6e816
Valentyn <valentynmaltsev@gmail.com>
2021-11-30 18:52:10 +0200

    Edit users

---
+++ src/Form/Handler/UserFormHandler.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Form\Handler;
+
+use App\Entity\User;
+use App\Utils\Manager\UserManager;
+use Symfony\Component\Form\Form;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFormHandler
+{
+    /**
+     * @var UserManager
+     */
+    private $userManager;
+    /**
+     * @var UserPasswordHasherInterface
+     */
+    private $userPasswordHasher;
+
+    public function __construct(UserManager $userManager, UserPasswordHasherInterface $userPasswordHasher)
+    {
+        $this->userManager = $userManager;
+        $this->userPasswordHasher = $userPasswordHasher;
+    }
+
+    /**
+     * @param User $user
+     * @return User|null
+     */
+    public function processEditForm(Form $form)
+    {
+        $plainPassword = $form->get('plainPassword')->getData();
+
+        /** @var User $user */
+        $user = $form->getData();
+
+        if ($plainPassword ) {
+            $encodedPassword = $this->userPasswordHasher->hashPassword($user, $plainPassword);
+            $user->setPassword($encodedPassword);
+        }
+
+        $this->userManager->save($user);
+
+        return $user;
+    }
+}
