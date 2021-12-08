9f2ab6b84d0b8f3cb53818a2ce61498555102267
Valentyn <valentynmaltsev@gmail.com>
2021-12-08 16:46:56 +0200

    Get products by uuid. Edit products (method PATCH)

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -31,13 +31,13 @@ class Category
      * @ORM\Id
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
-     * @Groups ({"category:list", "category:item", "product:list"})
+     * @Groups ({"category:list", "category:item", "product:list", "product:item"})
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=100)
-     * @Groups ({"category:list", "category:item", "product:list"})
+     * @Groups ({"category:list", "category:item", "product:list", "product:item"})
      */
     private $title;
 
